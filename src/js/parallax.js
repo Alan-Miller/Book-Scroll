3eff3a55e342dd7b124e7d9be6d1e1032f247484
Alan-Miller <alan.ashman@gmail.com>
2016-09-12 12:49:49 -0600

    monday morning

--- src/js/parallax.js
+++ src/js/parallax.js
@@ -6,7 +6,7 @@ $(window).scroll(function() {
 
   // $('.book-scroll-title').css({'transform': 'translate(-' + winScroll / 80 + '%, 0px)'});
 
-  $('.book-scroll-title').css({'transform' :'translate(0px, ' + winScroll / 3 + '%)'});
+  $('.book-scroll-title').css({'transform' :'translate(0px, ' + winScroll * -0.3 + '%)'});
 
   // $('.bo').css({'position': 'absolute', 'transform': 'rotate(-' + winScroll / 2 + 'deg)'});
   // $('.ok').css({'position': 'absolute', 'transform': 'rotate(-' + winScroll / 2 + 'deg)'});
